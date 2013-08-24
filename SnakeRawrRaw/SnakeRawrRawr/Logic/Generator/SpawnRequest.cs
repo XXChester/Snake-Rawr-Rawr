1817be0389fc76efdeed887a0afc8cdad141667a
mcculbr <mcculbr@8ec5e4ca-6654-e54a-8ea6-c57665c7f23a>
2013-08-24 18:49:39 +0000

    Release candidates and a POC
    git-svn-id: http://gwnp-server/repos/Programming/Work/C%23%20Gaming/XNA%20Projects/SnakeRawrRawr@1199 8ec5e4ca-6654-e54a-8ea6-c57665c7f23a

---
+++ SnakeRawrRaw/SnakeRawrRawr/Logic/Generator/SpawnRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace SnakeRawrRawr.Logic.Generator {
+	public class SpawnRequest {
+
+		public bool Surround { get; set; }
+		public bool MarkGeneratedPosition { get; set; }
+		public PositionGenerator.SpawnProcessor Processor { get; set; }
+
+		public SpawnRequest() { }
+		public SpawnRequest(PositionGenerator.SpawnProcessor processor, bool surround = true, bool markGeneratedPosition = true) {
+			this.Surround = surround;
+			this.MarkGeneratedPosition = markGeneratedPosition;
+			this.Processor = processor;
+		}
+	}
+}
